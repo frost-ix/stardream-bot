cf90b1812f92bddee4e2a2b325e3164c9476b297
frost-ix <shw9810@icloud.com>
2025-10-02 11:50:30 +0900

    fix : command regex (2)

--- src/commands/linkToX.ts
+++ src/commands/linkToX.ts
@@ -6,7 +6,7 @@ import {
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("X링크")
+    .setName("링크")
     .setDescription("스티드림 멤버들의 X(구 트위터) 링크를 안내합니다."),
   async execute(interaction: any) {
     if (!interaction.isChatInputCommand()) return;
